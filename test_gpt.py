a5b06075bdbb36a292c304f64fabb9a075f91c2f
eds53323 <e.ds53323@gmail.com>
2023-04-02 21:38:08 +0800

    add new file

---
+++ test_gpt.py
@@ -0,0 +1,18 @@
+import os
+import openai
+
+openai.api_key = "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"
+
+messages=[
+        {"role": "system", "content": "you're a doctor"},
+]
+def chat(text):
+    global messages
+    messages.append({"role": "user", "content": text})
+    return openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=messages
+    )["choices"][0]["message"]["content"]
+
+result = chat( "血壓量測結果 高血壓300 低血壓30 是否正常? 有甚麼須注意的事情")
+print(result)
